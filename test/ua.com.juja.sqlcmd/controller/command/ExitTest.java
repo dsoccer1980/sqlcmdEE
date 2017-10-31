41db14e793aa4ccfd58e38752cdc6649abad2d70
dsoccer1980 <dsoccer1980@gmail.com>
2017-10-31 21:42:32 +0200

    ExitTest with FakeView

---
+++ test/ua.com.juja.sqlcmd/controller/command/ExitTest.java
@@ -0,0 +1,54 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExitTest {
+
+    private FakeView view = new FakeView();
+
+    @Test
+    public void testCanProcessExitString(){
+        //given
+        Command command = new Exit(view);
+
+        //when
+        boolean canProcess = command.canProcess("exit");
+
+        //then
+        assertTrue(canProcess);
+    }
+
+    @Test
+    public void testCantProcessExitQweString(){
+        //given
+        Command command = new Exit(view);
+
+        //when
+        boolean canProcess = command.canProcess("qwe");
+
+        //then
+        assertFalse(canProcess);
+    }
+
+    @Test
+    public void testProcessExitCommand_ThrowsExitException(){
+        //given
+        Command command = new Exit(view);
+
+        //when
+        try {
+            command.process("exit");
+            fail("Expected ExitException");
+        }
+        catch (ExitException e){
+            //do nothing
+        }
+
+        //then
+        assertEquals("До скорой встречи!\n", view.getContent());
+        //throws ExitException
+    }
+}
