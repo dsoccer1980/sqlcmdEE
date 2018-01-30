3c0094acc64a8524b081d44a596ba1df1b92f3e1
dsoccer1980 <dsoccer1980@gmail.com>
2018-01-30 19:58:30 +0200

    Added ServiceImplTest

---
+++ src/test/java/ua/com/juja/sqlcmd/service/ServiceImplTest.java
@@ -0,0 +1,20 @@
+package ua.com.juja.sqlcmd.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by denis on 30.01.2018.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/test/java/resources/test-application-context.xml" })
+public class ServiceImplTest {
+
+    @Test
+    public void test(){
+
+    }
+}
