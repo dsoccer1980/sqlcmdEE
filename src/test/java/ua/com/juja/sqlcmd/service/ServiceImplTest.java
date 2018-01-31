0ebc5b69c48ee8ef5f6f8790f1618cf1c6428697
dsoccer1980 <dsoccer1980@gmail.com>
2018-01-31 19:25:57 +0200

    Created test using @RunWith(SpringJUnit4ClassRunner.class)

--- src/test/java/ua/com/juja/sqlcmd/service/ServiceImplTest.java
+++ src/test/java/ua/com/juja/sqlcmd/service/ServiceImplTest.java
@@ -2,19 +2,34 @@ package ua.com.juja.sqlcmd.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.com.juja.sqlcmd.model.DataSetImpl;
+import ua.com.juja.sqlcmd.model.DatabaseManager;
 
-/**
- * Created by denis on 30.01.2018.
- */
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/test/java/resources/test-application-context.xml" })
+@ContextConfiguration(locations = { "/test-application-context.xml" })
 public class ServiceImplTest {
+
+    @Autowired
+    private Service service;
 
     @Test
     public void test(){
+        //given
+        DatabaseManager manager = service.connect("sqlcmd", "postgres", "postgres");
+
+        //when
+        List<List<String>> users = service.find(manager, "users");
+
+        //then
+        assertEquals("[[name, password, id], [denis, ***, 1]]", users.toString());
 
     }
 }
