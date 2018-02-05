5cb65232bdd35327729e13d14415d15671fa64ad
dsoccer1980 <dsoccer1980@gmail.com>
2018-02-05 19:54:29 +0200

    Added menu command

--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -1,14 +1,29 @@
 package ua.com.juja.sqlcmd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.juja.sqlcmd.service.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private Service service;
 
     @RequestMapping(value = "/help", method = RequestMethod.GET)
     public String help() {
         return "help";
     }
+
+    @RequestMapping(value = "/menu", method = RequestMethod.GET)
+    public String menu(HttpServletRequest request) {
+        request.setAttribute("items", service.commandsList());
+        return "/menu";
+    }
+
+
 }
