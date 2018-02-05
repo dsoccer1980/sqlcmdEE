9c5fed1e9e0892d6665c41f6dbed8c7ad46c2d2f
Deniss <dsoccer1980@gmail.com>
2018-02-05 15:09:15 +0200

    attempt to add reflection

--- src/main/java/ua/com/juja/sqlcmd/controller/web/ActionResolver.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/web/ActionResolver.java
@@ -1,5 +1,6 @@
 package ua.com.juja.sqlcmd.controller.web;
 
+import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ua.com.juja.sqlcmd.controller.web.Action;
@@ -7,17 +8,35 @@ import ua.com.juja.sqlcmd.controller.web.actions.*;
 import ua.com.juja.sqlcmd.service.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class ActionResolver {
 
     @Autowired
     private Service service;
 
-    private List<Action> actions = new ArrayList<>();
+    private List<Action> actions;
 
     public ActionResolver() {
+        actions  = new ArrayList<>();
+
+//        Reflections reflections = new Reflections(ErrorAction.class.getPackage().getName());
+//        Set<Class<? extends AbstractAction>> classes = reflections.getSubTypesOf(AbstractAction.class);
+//
+//        for (Class<? extends AbstractAction> aClass : classes) {
+//            if (aClass.equals(ErrorAction.class)) {
+//                continue;
+//            }
+//            try {
+//                AbstractAction action = aClass.getConstructor().newInstance(service.getClass());
+//                actions.add(action);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
         actions.add(new ConnectAction());
         actions.add(new MenuAction());
         actions.add(new HelpAction());
