b83e0545b36a8a6bcbc6c2e16815964ae78a88d0
dsoccer1980 <dsoccer1980@gmail.com>
2018-02-05 19:34:04 +0200

    Changing architecture, based on SpringMvc

---
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -0,0 +1,14 @@
+package ua.com.juja.sqlcmd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/help", method = RequestMethod.GET)
+    public String help() {
+        return "help";
+    }
+}
