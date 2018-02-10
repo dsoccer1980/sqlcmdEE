29bdc472032ff9e3d45f70d2fb0053f9c75f4313
dsoccer1980 <dsoccer1980@gmail.com>
2018-02-10 19:39:42 +0200

    Added Transactional

--- src/main/java/ua/com/juja/sqlcmd/model/UserActionRepositoryImpl.java
+++ src/main/java/ua/com/juja/sqlcmd/model/UserActionRepositoryImpl.java
@@ -1,23 +1,28 @@
 package ua.com.juja.sqlcmd.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import ua.com.juja.sqlcmd.model.entity.DatabaseConnection;
 import ua.com.juja.sqlcmd.model.entity.UserAction;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 public class UserActionRepositoryImpl implements UserActionRepositoryCustom {
 
-    @Autowired
-    private UserActionRepository userActions;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     private DatabaseConnectionRepository databaseConnections;
 
     @Override
+    @Transactional
     public void saveAction(String databaseName, String userName, String action) {
         DatabaseConnection databaseConnection = databaseConnections.findByUserNameAndDbName(userName, databaseName);
         if (databaseConnection == null) {
             databaseConnection = databaseConnections.save(new DatabaseConnection(userName, databaseName));
         }
-        userActions.save(new UserAction(action, databaseConnection));
+        entityManager.persist(new UserAction(action, databaseConnection));
     }
 }
